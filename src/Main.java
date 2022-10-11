0697d386d9ddbbc458802698680d64ebdab97ee5
Anastasia <a.grebenkina93@gmail.com>
2022-10-11 10:30:09 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int money = 100;
+        int popolnenir = 1000;
+
+
+        if (popolnenir > 1000) {
+            System.out.println(money + popolnenir + (popolnenir * 10 / 1000));
+        } else {
+            System.out.println(money + popolnenir);
+
+
+        }
+    }
+}
